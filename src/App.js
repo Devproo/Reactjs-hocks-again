9b58bf84ab0911b7ff8bd5a380db07b0adf7cfe6
ipeerless <ipeerless0@gmail.com>
2022-08-30 22:35:12 +0200

    dynamic windo

--- src/App.js
+++ src/App.js
@@ -3,34 +3,20 @@ import './App.css';
 
 
 export default function App() {
- const [resourceType, setResourceType] = useState('posts')
- const [items, setItems] = useState([])
- console.log('render')
- useEffect(() => {
-  // eslint-disable-next-line no-template-curly-in-string
-  fetch('https://jsonplaceholder.typicode.com/${resourceType}')
-  .then(response => response.json())
-  .then(json => setItems(json))
+ const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+const handleResize = () => {
+setWindowWidth(window.innerWidth)
 
+}
 
- }, [resourceType])
+useEffect(() => {
+
+window.addEventListener('resize', handleResize)
+
+}, [])
  
- 
-
-
-  return (
-    <div >
-      <button onClick={() => setResourceType('posts')}>Posts</button>
-      <button onClick={() => setResourceType('users')}>Users</button>
-      <button onClick={() => setResourceType('comments')}>Comments</button>
-      
-      <hi>{resourceType}</hi>
-     {items.map(items => {
-
-      return <pre>{JSON.stringify(items)}</pre>
-     })}
-     
-    </div>
+return (
+    <div > {windowWidth} </div>
   )
   }
 
