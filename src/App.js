c97ecfce707ba51a56835e6365ff6dbdb9556ef0
ipeerless <ipeerless0@gmail.com>
2022-08-30 23:33:38 +0200

    Reactjs useMemo hock

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useMemo, useEffect} from 'react';
 import './App.css';
 
 
 export default function App() {
- const [windowWidth, setWindowWidth] = useState(window.innerWidth)
-const handleResize = () => {
-setWindowWidth(window.innerWidth)
-
+ const [number, setNumber] = useState(0)
+ const [dark, setDark] = useState(false)
+ const doubleNumber = useMemo(() => {
+ return slowFunction(number)
+}, [number])
+ const themeStyles = useMemo(() => {
+return {
+  backgroundColor: dark ? 'dark' : 'white',
+  color: dark ? 'white' : 'black'
 }
+
+ }, [dark])
+ 
+ 
+  
+
 
 useEffect(() => {
-
-window.addEventListener('resize', handleResize)
-return () => {
+console.log('theme changed')
 
 
-  window.removeEventListener('resize', handleResize)
-}
+}, [themeStyles])
 
-
-
-
-}, [])
- 
 return (
-    <div > {windowWidth} </div>
-  )
-  }
+    <> 
+      <input type="number" value={number} onChange={e => setNumber(parseInt(e.target.value))} />
+      <button onClick={ () => setDark(prevDark => !prevDark)} > change theme</button>
+      <div style={themeStyles}>{doubleNumber}</div>
 
+       </>
+  )
+  function slowFunction(num) {
+
+    console.log('calling slow func')
+    for (let i = 0; i <= 1000000; i++) {}
+    return num * 5
+  }
+}
 
